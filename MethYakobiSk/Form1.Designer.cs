068431d43ec03ecd637f5c6f825636376fdd479a
MrTokin <matttokin@gmail.com>
2022-02-28 15:24:57 +0300

    Initial commit

---
+++ MethYakobiSk/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace MethYakobiSk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 40);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(411, 367);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите количество переменных :";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(202, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(54, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.AllowUserToAddRows = false;
+            this.dataGridView3.AllowUserToDeleteRows = false;
+            this.dataGridView3.AllowUserToResizeColumns = false;
+            this.dataGridView3.AllowUserToResizeRows = false;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(436, 40);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowHeadersVisible = false;
+            this.dataGridView3.Size = new System.Drawing.Size(360, 43);
+            this.dataGridView3.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(433, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Результат";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(527, 115);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(177, 33);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Расчитать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(433, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Точность:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(501, 89);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "0,001";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Решение СЛАУ - Метод Якоби";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ MethYakobiSk/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MethYakobiSk
+{
+    public partial class Form1 : Form
+    {
+        int countCol;
+        double eps; /// желаемая точность
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int[] cmbValues = new int[] { 3, 4, 5, 6, 7, 8 };
+            foreach (var item in cmbValues)
+            {
+                comboBox1.Items.Add(item);
+            }
+            comboBox1.SelectedIndex = 0;
+
+            drawMatrix();
+        }
+        private void drawMatrix()
+        {
+            dataGridView1.Columns.Clear();
+            dataGridView3.Columns.Clear();
+
+            countCol = int.Parse(comboBox1.SelectedItem.ToString());
+            if (countCol > 8 || countCol < 3)
+            {
+                MessageBox.Show("Произошла ошибка");
+                return; 
+            }
+
+            int widthMatrixValue = dataGridView1.Width;
+            int widthColMV = (widthMatrixValue - 1) / (countCol + 1);
+            
+            DataGridViewTextBoxColumn[] columnGr1 = new DataGridViewTextBoxColumn[countCol + 1];
+            DataGridViewTextBoxColumn[] columnGr3 = new DataGridViewTextBoxColumn[countCol];
+
+            for (int i = 0; i <= countCol; i++)
+            {
+                columnGr1[i] = new DataGridViewTextBoxColumn();
+                
+                if (i == countCol)
+                {
+                    columnGr1[i].HeaderText = "B";
+                    columnGr1[i].Name = "B" + i;
+                    columnGr1[i].Width = widthMatrixValue - countCol * widthColMV - 3;
+
+                }
+                else
+                {
+                    columnGr1[i].HeaderText = "X" + (i + 1);
+                    columnGr1[i].Name = "X" + (i + 1);
+                    columnGr1[i].Width = widthColMV;
+                }
+
+                if (i < countCol)
+                {
+                    columnGr3[i] = new DataGridViewTextBoxColumn();
+                    columnGr3[i].HeaderText = "X" + (i + 1);
+                    columnGr3[i].Name = "X" + (i + 1);
+                    if (i == countCol - 1)
+                    {
+                        columnGr3[i].Width = widthMatrixValue - (countCol - 1) * widthColMV - 3;
+                    }
+                    else
+                    {
+                        columnGr3[i].Width = widthColMV;
+                    }
+                }
+            }
+
+            widthMatrixValue = dataGridView3.Width;
+            widthColMV = (widthMatrixValue - 1) / countCol;
+
+            for (int i = 0; i < countCol; i++)
+            {
+                columnGr3[i] = new DataGridViewTextBoxColumn();
+                columnGr3[i].HeaderText = "X" + (i + 1);
+                columnGr3[i].Name = "X" + (i + 1);
+                if (i == countCol - 1)
+                {
+                    columnGr3[i].Width = widthMatrixValue - (countCol - 1) * widthColMV - 3;
+                }
+                else
+                {
+                    columnGr3[i].Width = widthColMV;
+                }
+            }
+
+            dataGridView1.Columns.AddRange(columnGr1);
+            dataGridView3.Columns.AddRange(columnGr3);
+
+            for (int i = 0; i < countCol; i++)
+            {
+                dataGridView1.Rows.Add();
+            }
+            dataGridView3.Rows.Add();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            drawMatrix();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double.TryParse(textBox1.Text, out eps);
+            if(eps <= 0)
+            {
+                eps = 0.001;
+            }
+
+            int[,] arr = new int[countCol, countCol];
+            double[,] A1 = new double[countCol, countCol];
+            
+            for(int i = 0; i < countCol; i++)
+            {
+                for (int j = 0; j < countCol; j++)
+                {
+                    double valueCells;
+                    double.TryParse(dataGridView1.Rows[i].Cells[j].Value?.ToString() ?? "0", out valueCells);
+                    A1[i,j] = valueCells;
+                }
+            }
+
+            double[] F1 = new double[countCol];
+            for (int j = 0; j < countCol; j++)
+            {
+                double valueCells;
+                double.TryParse(dataGridView1.Rows[j].Cells[countCol].Value?.ToString() ?? "0", out valueCells);
+                F1[j] = valueCells;
+            }
+
+            double[] X1 = new double[countCol];
+
+            var result = Jacobi(countCol, A1, F1, X1);
+            for (int i = 0; i < result.Count(); i++)
+            {
+                dataGridView3.Rows[0].Cells[i].Value = result[i];
+            }
+        }
+        
+
+        /// N - размерность матрицы; 
+        /// A[N][N] - матрица коэффициентов
+        /// F[N] - столбец свободных членов
+        /// X[N] - начальное приближение, ответ записывается также в X[N]
+        double[] Jacobi(int N, double[,] A, double[] F, double[] X)
+        {
+            double[] TempX = new double[N];
+            double norm; // норма, определяемая как наибольшая разность компонент столбца иксов соседних итераций.
+
+            do
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    TempX[i] = F[i];
+                    for (int g = 0; g < N; g++)
+                    {
+                        if (i != g)
+                        {
+                            TempX[i] -= A[i,g] * X[g];
+                        }
+                    }
+                    TempX[i] /= A[i,i];
+                }
+                norm = Math.Abs(Math.Abs(X[0]) - Math.Abs(TempX[0]));
+                for (int h = 0; h < N; h++)
+                {
+                    if (Math.Abs(Math.Abs(X[h]) - Math.Abs(TempX[h])) > norm)
+                    {
+                        norm = Math.Abs(Math.Abs(X[h]) - Math.Abs(TempX[h]));
+                    }
+                    X[h] = TempX[h];
+                }
+                //Console.WriteLine(X[0] + " " + X[1] + " " + X[2] + " " + X[3]);
+            } while (norm > eps);
+            TempX = null;
+
+            return X;
+        }
+    }
+}
